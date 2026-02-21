ae6ba93cc2978e74e72c8812f6dc5bf6aa6e92b2
Eka38b <166035094+Eka38b@users.noreply.github.com>
2026-02-22 03:53:29 +0900

    Add files via upload

---
+++ on_Model/__init__.py
@@ -0,0 +1,6 @@
+"""Package initializer for `on_Model`.
+
+This package is part of the *Information_dynamics* project and contains
+models (e.g., ABN/GRN examples) plus the reusable `InfoDyn_lib` toolkit.
+"""
+
