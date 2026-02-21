321150bce7f8acb83e643e3956d3d8d72442ee4d
Eka38b <166035094+Eka38b@users.noreply.github.com>
2026-02-22 03:56:24 +0900

    Add files via upload

--- on_Model/InfoDyn_lib/Estimators/__init__.py
+++ on_Model/InfoDyn_lib/Estimators/__init__.py
@@ -1,1 +1,6 @@
+"""Package initializer for `InfoDyn_lib/Estimators`.
+
+This package is part of the *Information_dynamics* project and contains
+models (e.g., ABN/GRN examples) plus the reusable `InfoDyn_lib` toolkit.
+"""
 
