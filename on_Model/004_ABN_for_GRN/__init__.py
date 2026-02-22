5235b56469b27e7e264d27aad03a6f2ebb87d62f
Eka38b <166035094+Eka38b@users.noreply.github.com>
2026-02-22 13:26:18 +0900

    Update __init__.py

--- on_Model/004_ABN_for_GRN/__init__.py
+++ on_Model/004_ABN_for_GRN/__init__.py
@@ -1,4 +1,4 @@
-"""Package initializer for `004_ABN_model_of_GRN`.
+"""Package initializer for `004_ABN_for_GRN`.
 
 This package is part of the *Information_dynamics* project and contains
 models (e.g., ABN/GRN examples) plus the reusable `InfoDyn_lib` toolkit.
